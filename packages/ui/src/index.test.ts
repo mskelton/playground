e2105958d5e594760cc6448b0ca325e269b910b0
Mark Skelton <mark@federato.ai>
2024-04-10 14:25:54 -0500

    Build

---
+++ packages/ui/src/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest"
+
+test("will pass", () => {
+  expect(1).toBe(1)
+})
---
+++ packages/utils/src/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest"
+
+test("will fail", () => {
+  expect(1).toBe(2)
+})
