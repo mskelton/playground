c40ae4f67bcdcd81cf870e2bb695352ac340a7b9
Mark Skelton <mdskelton99@gmail.com>
2024-09-30 17:21:02 -0500

    Sync

--- test/hi.spec.ts
+++ test/hi.spec.ts
@@ -3,11 +3,3 @@ import { test, expect } from "@playwright/test"
 test("passing", () => {
   expect(1).toBe(1)
 })
-
-test("failing", () => {
-  expect(1).toBe(2)
-})
-
-test("flaky", ({}, testInfo) => {
-  expect(testInfo.retry).toBe(1)
-})
